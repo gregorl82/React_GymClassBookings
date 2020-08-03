a3f087729c407b60b713c9d69ff36c0e46a26638
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-08-03 19:36:10 +0100

    Updates server

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get("/api/test", (req, res) => {
+  res.send({ message: "Hello world" });
+});
+
 app.listen(5000, () => {
   console.log("Listening on port 5000");
 });
