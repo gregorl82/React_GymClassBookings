3157220092deb5a2a1bb355f56c0b99f361b7fed
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-08-04 20:39:24 +0100

    Rearrange server file

--- server/server.js
+++ server/server.js
@@ -2,14 +2,12 @@ const Koa = require("koa");
 const KoaRouter = require("koa-router");
 const json = require("koa-json");
 
+const PORT = 5000;
+
 const app = new Koa();
 const router = new KoaRouter();
 
-const PORT = 5000;
-
 app.use(json());
-
-// app.use(async (ctx) => (ctx.body = { message: "Hello world" }));
 
 router.get("/test", (ctx) => (ctx.body = { message: "Hello world" }));
 
